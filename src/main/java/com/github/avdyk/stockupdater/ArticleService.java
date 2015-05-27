61268ceb541c3f3300fb0973962bd9faabc13888
Arnaud Vandyck <avd@mims.be>
2015-05-27 17:42:43 +0200

    Added pre-required verification

--- src/main/java/com/github/avdyk/stockupdater/ArticleService.java
+++ src/main/java/com/github/avdyk/stockupdater/ArticleService.java
@@ -1,5 +1,6 @@
 package com.github.avdyk.stockupdater;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -13,7 +14,9 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Update the database with the StockCompute.
@@ -35,13 +38,31 @@ public class ArticleService {
                           final String sheetName,
                           final String out,
                           final String... in) throws IOException {
-      final XSSFWorkbook workbook = new XSSFWorkbook(Files.newInputStream(excelFile));
-      if (LOG.isDebugEnabled()) {
-        final int sheets = workbook.getNumberOfSheets();
-        for (int i = 0; i < sheets; i++) {
-          LOG.debug("Sheet {}: {}", i, workbook.getSheetName(i));
-        }
+    // pre-requis:
+    if (StringUtils.isBlank(out)) {
+      throw new IllegalArgumentException("Unknown 'out' column to update the stock");
+    }
+    if (in == null || in.length == 0) {
+      throw new IllegalArgumentException("Must have at least one 'in' column to lookup the barcode");
+    }
+    for (String i : in) {
+      if (StringUtils.isBlank(i)) {
+        throw new IllegalArgumentException("Illegal value for one of the 'in' column");
       }
+    }
+    if (stock == null) {
+      throw new NullPointerException("Stock cannot be 'null'");
+    }
+    if (updateType == null) {
+      throw new NullPointerException("Update Type cannot be 'null'");
+    }
+    final XSSFWorkbook workbook = new XSSFWorkbook(Files.newInputStream(excelFile));
+    if (LOG.isDebugEnabled()) {
+      final int sheets = workbook.getNumberOfSheets();
+      for (int i = 0; i < sheets; i++) {
+        LOG.debug("Sheet {}: {}", i, workbook.getSheetName(i));
+      }
+    }
     final XSSFSheet sheet;
     if (sheetName != null) {
       sheet = workbook.getSheet(sheetName);
@@ -50,6 +71,8 @@ public class ArticleService {
       sheet = workbook.getSheetAt(0);
       LOG.debug("No sheet name, picking the first one");
     }
+    final int outIndex;
+    final List<Integer> intIndexes = new ArrayList<>();
     final Iterator<Row> rowIter = sheet.rowIterator();
     if (rowIter != null && rowIter.hasNext()) {
       final Row header = rowIter.next();
