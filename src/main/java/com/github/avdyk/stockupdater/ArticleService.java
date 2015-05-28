f07286090b91edaaeca1bbad25facaeed1b3a95b
Arnaud Vandyck <avd@mims.be>
2015-05-28 16:13:35 +0200

    Load xlsx file only once

--- src/main/java/com/github/avdyk/stockupdater/ArticleService.java
+++ src/main/java/com/github/avdyk/stockupdater/ArticleService.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -32,10 +33,12 @@ public class ArticleService {
 
   private static final Logger LOG = LoggerFactory.getLogger(ArticleService.class);
 
-  final Path excelFile;
-  final String sheetName;
-  final String out;
-  final String[] in;
+  Path excelFile;
+  String sheetName;
+  String out;
+  List<String> in;
+  int outIndex;
+  List<Integer> intIndexes = new ArrayList<>();
 
   @Autowired
   public ArticleService(@Value("#{confImpl.excelFile}") final Path excelFile,
@@ -57,7 +60,7 @@ public class ArticleService {
     this.excelFile = excelFile;
     this.sheetName = sheetName;
     this.out = out;
-    this.in = in;
+    this.in = Arrays.asList(in);
     final XSSFWorkbook workbook = new XSSFWorkbook(Files.newInputStream(excelFile));
     if (LOG.isDebugEnabled()) {
       final int sheets = workbook.getNumberOfSheets();
@@ -73,13 +76,36 @@ public class ArticleService {
       sheet = workbook.getSheetAt(0);
       LOG.debug("No sheet name, picking the first one");
     }
-    final int outIndex;
-    final List<Integer> intIndexes = new ArrayList<>();
+    int outIndexTemp = -1;
     final Iterator<Row> rowIter = sheet.rowIterator();
     if (rowIter != null && rowIter.hasNext()) {
       final Row header = rowIter.next();
-      final Iterator<Cell> cellIter = header.iterator();
-      // TODO parcourir les colonnes pour trouver la out et toutes les in
+      // parcourir les colonnes pour trouver la out
+      final Iterator<Cell> cellIterator = header.iterator();
+      while (outIndexTemp == -1 && cellIterator.hasNext()) {
+        final Cell c = cellIterator.next();
+        outIndexTemp = out.equals(c.getStringCellValue()) ? c.getColumnIndex() : -1;
+      }
+      if (outIndexTemp == -1) {
+        // TODO try to add new column instead of throwing an exception
+        throw new IllegalArgumentException("out column not found");
+      }
+      outIndex = outIndexTemp;
+      // parcourir les colonnes pour trouver la out et toutes les in
+      final List<String> inTemp = new ArrayList<>(this.in);
+      for (final Cell cell : header) {
+        final String cellName = cell.getStringCellValue();
+        final int cellIndex = cell.getColumnIndex();
+        if (inTemp.contains(cellName)) {
+          this.intIndexes.add(cellIndex);
+          inTemp.remove(cellName);
+        }
+      }
+      if (!inTemp.isEmpty()) {
+        for (final String i : inTemp) {
+          LOG.warn("Column {} has not been found", i);
+        }
+      }
     }
   }
 
@@ -91,5 +117,6 @@ public class ArticleService {
     if (updateType == null) {
       throw new NullPointerException("Update Type cannot be 'null'");
     }
+    LOG.info("Updating stock");
   }
 }
