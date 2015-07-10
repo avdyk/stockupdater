aa8142295a2532c9739cdadb7b1cb7f5d79afc25
Arnaud Vandyck <avandyck@gmail.com>
2015-07-10 14:32:57 +0200

    Added excell out file, stock column name; added setXXX methods; refactor to use the setXXX methods

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
 # stockupdater
 Update the stock from a flat file with numerics in an Excel sheet
 
+## Install
+
 Install Java [JDK8](http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html) minimum.
 
 Use [maven](http://maven.apache.org/download.cgi) to compile, package or launch the project.
 
+## Launch
+
 Example to launch the tests:
 
-`$ mvn exec:java -Dstock.file=src/test/resources/stock.txt -Dexcel.file=src/test/resources/articles.xls -Dupdate.type=UPDATE -Dexcel.sheetname="my export articles" -Dexcel.in=s_id -Dexcel.out=new_stock`
+`$ mvn exec:java -Dstock.file=src/test/resources/stock.txt -Dexcel.file.in=src/test/resources/articles.xls -Dupdate
+.type=UPDATE -Dexcel.sheetname="my export articles" -Dexcel.in=s_id -Dexcel.out=new_stock`
+
+## Properties:
+
+* excel.file.in
+* excel.file.out
+* excel.sheetname
+* excel.stock
+* excel.out
+* excel.in
+* stock.file
+* update.type
+
--- src/main/java/com/github/avdyk/stockupdater/conf/ConfImpl.java
+++ src/main/java/com/github/avdyk/stockupdater/conf/ConfImpl.java
@@ -15,78 +15,145 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Configuration
-@ComponentScan(basePackages = { "com.github.avdyk.stockupdater" })
+@ComponentScan(basePackages = {"com.github.avdyk.stockupdater"})
 public class ConfImpl {
 
-	private final static Logger LOG = LoggerFactory.getLogger(ConfImpl.class);
-  public static final String EXCEL_FILE = "excel.file";
-  public static final String EXCEL_SHEETNAME = "excel.sheetname";
-  public static final String EXCEL_OUT_COLUMN_NAME = "excel.out";
-  public static final String EXCEL_IN_COLUMN_NAMES = "excel.in";
-  public static final String STOCK_FILE = "stock.file";
-  public static final String UPDATE_TYPE = "update.type";
+    private final static Logger LOG = LoggerFactory.getLogger(ConfImpl.class);
+    public static final String EXCEL_FILE_IN = "excel.file.in";
+    public static final String EXCEL_FILE_OUT = "excel.file.out";
+    public static final String EXCEL_SHEETNAME = "excel.sheetname";
+    public static final String EXCEL_STOCK_COLUMN_NAME = "excel.stock";
+    public static final String EXCEL_OUT_COLUMN_NAME = "excel.out";
+    public static final String EXCEL_IN_COLUMN_NAMES = "excel.in";
+    public static final String STOCK_FILE = "stock.file";
+    public static final String UPDATE_TYPE = "update.type";
 
-  @Autowired
-	private Environment env;
-  private Path excelFile;
-  private Path stockFile;
-  private UpdateType updateType;
-  private String sheetName;
-  private String out;
-  private String[] in;
+    @Autowired
+    private Environment env;
+    private Path excelFileIn;
+    private Path excelFileOut;
+    private Path stockFile;
+    private UpdateType updateType;
+    private String sheetName;
+    private String stockColumn;
+    private String out;
+    private String[] in;
 
-  @PostConstruct
-  void postConstruct() {
-    final String exFstr = env.getProperty(EXCEL_FILE);
-    excelFile = Paths.get(exFstr);
-    LOG.debug("Excel filename: {}", excelFile);
-    sheetName = env.getProperty(EXCEL_SHEETNAME);
-    LOG.debug("Excel sheetname: {}", sheetName);
-    out = env.getProperty(EXCEL_OUT_COLUMN_NAME);
-    LOG.debug("Excel out column name: {}", out);
-    final String instr = env.getProperty(EXCEL_IN_COLUMN_NAMES);
-    in = instr.split(",");
-    LOG.debug("Excel in column names: {}", in);
-    final String stFstr = env.getProperty(STOCK_FILE);
-    stockFile = Paths.get(stFstr);
-    LOG.debug("Stock filename: {}", stockFile);
-    final String upTstr = env.getProperty(UPDATE_TYPE);
-    updateType = UpdateType.valueOf(upTstr);
-    LOG.debug("Update type: {}", updateType);
-  }
+    @PostConstruct
+    void postConstruct() {
+        // EXCEL_FILE_IN
+        final String exFstr = env.getProperty(EXCEL_FILE_IN);
+        if (exFstr != null) {
+            this.setExcelFileIn(Paths.get(exFstr));
+        }
+        LOG.debug("Excel in filename: {}", excelFileIn);
+        // EXCEL_FILE_OUT
+        final String exFOutstr = env.getProperty(EXCEL_FILE_OUT);
+        if (exFOutstr != null) {
+            this.setExcelFileOut(Paths.get(exFOutstr));
+        }
+        LOG.debug("Excel out filename: {}", excelFileOut);
+        // EXCEL_SHEETNAME
+        this.setSheetName(env.getProperty(EXCEL_SHEETNAME));
+        LOG.debug("Excel sheetname: {}", sheetName);
+        // EXCEL_STOCK_COLUMN_NAME
+        this.setStockColumn(EXCEL_STOCK_COLUMN_NAME);
+        LOG.debug("Excel stock column name: {}", EXCEL_STOCK_COLUMN_NAME);
+        // EXCEL_OUT_COLUMN_NAME
+        this.setOut(env.getProperty(EXCEL_OUT_COLUMN_NAME));
+        LOG.debug("Excel out column name: {}", out);
+        // EXCEL_IN_COLUMN_NAMES
+        final String instr = env.getProperty(EXCEL_IN_COLUMN_NAMES);
+        if (instr != null) {
+            this.setIn(instr.split(","));
+        }
+        LOG.debug("Excel in column names: {}", in);
+        // STOCK_FILE
+        final String stFstr = env.getProperty(STOCK_FILE);
+        if (stFstr != null) {
+            this.setStockFile(Paths.get(stFstr));
+        }
+        LOG.debug("Stock filename: {}", stockFile);
+        // UPDATE_TYPE
+        final String upTstr = env.getProperty(UPDATE_TYPE);
+        if (upTstr != null) {
+            updateType = UpdateType.valueOf(upTstr);
+        }
+        LOG.debug("Update type: {}", updateType);
+    }
 
-	public Path getExcelFile() {
-		return excelFile;
-	}
+    public Path getExcelFileIn() {
+        return excelFileIn;
+    }
 
-	public Path getStockFile() {
-		return stockFile;
-	}
+    public void setExcelFileIn(Path excelFileIn) {
+        this.excelFileIn = excelFileIn;
+    }
 
-	public UpdateType getUpdateType() {
-		return updateType;
-	}
+    public Path getExcelFileOut() {
+        return excelFileOut;
+    }
 
-  public String getSheetName() {
-    return sheetName;
-  }
+    public void setExcelFileOut(Path excelFileOut) {
+        this.excelFileOut = excelFileOut;
+    }
 
-  public String getOut() {
-    return out;
-  }
+    public Path getStockFile() {
+        return stockFile;
+    }
 
-  public String[] getIn() {
-    return in;
-  }
+    public void setStockFile(final Path stockFile) {
+        this.stockFile = stockFile;
+    }
 
-  /**
-	 * To resolve ${} in @Value.
-	 *
-	 * @return a property sources place holder configurer for Spring.
-	 */
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
-		return new PropertySourcesPlaceholderConfigurer();
-	}
+    public UpdateType getUpdateType() {
+        return updateType;
+    }
+
+    public void setUpdateType(final UpdateType updateType) {
+        this.updateType = updateType;
+    }
+
+    public String getSheetName() {
+        return sheetName;
+    }
+
+    public void setSheetName(final String sheetName) {
+        this.sheetName = sheetName;
+    }
+
+    public String getStockColumn() {
+        return stockColumn;
+    }
+
+    public void setStockColumn(String stockColumn) {
+        this.stockColumn = stockColumn;
+    }
+
+    public String getOut() {
+        return out;
+    }
+
+    public void setOut(String out) {
+        this.out = out;
+    }
+
+    public String[] getIn() {
+        return in;
+    }
+
+    public void setIn(String[] in) {
+        this.in = in;
+    }
+
+    /**
+     * To resolve ${} in @Value.
+     *
+     * @return a property sources place holder configurer for Spring.
+     */
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
 }
