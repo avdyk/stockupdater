a5a2e00c608c65eb2bfcbc92a937a857456bdbb8
Arnaud Vandyck <avandyck@gmail.com>
2015-08-18 00:23:16 +0200

    Initial commit of the JavaFX UI

---
+++ src/main/java/com/github/avdyk/stockupdater/ui/javafx/StockUpdater.java
@@ -0,0 +1,21 @@
+package com.github.avdyk.stockupdater.ui.javafx;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Classe de lancement de l'application JavaFX.
+ *
+ * @author <a href="mailto:avandyck@gmail.com">Arnaud Vandyck</a>
+ * @version 1.0, 18/08/15.
+ */
+public class StockUpdater {
+
+  private static final Logger logger = LoggerFactory.getLogger(StockUpdater.class);
+
+  public static void main(String[] args) {
+    logger.info("StockUpdater starting...");
+
+  }
+
+}
