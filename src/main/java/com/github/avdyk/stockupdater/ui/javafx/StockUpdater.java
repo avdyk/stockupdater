4aad303867de6320fe4ec9061502e7a58956b382
Arnaud Vandyck <avandyck@gmail.com>
2015-08-19 23:12:59 +0200

    Added a default handler for unexpected exceptions

--- src/main/java/com/github/avdyk/stockupdater/ui/javafx/StockUpdater.java
+++ src/main/java/com/github/avdyk/stockupdater/ui/javafx/StockUpdater.java
@@ -4,10 +4,18 @@ import com.github.avdyk.stockupdater.conf.ConfImpl;
 import com.github.avdyk.stockupdater.ui.javafx.controller.MainFrameController;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 /**
  * Classe de lancement de l'application JavaFX.
@@ -28,6 +36,7 @@ public class StockUpdater extends Application {
   public void start(Stage primaryStage) throws Exception {
     AnnotationConfigApplicationContext context
         = new AnnotationConfigApplicationContext(ConfImpl.class);
+    Thread.setDefaultUncaughtExceptionHandler(this::exceptionHandler);
     MainFrameController controller = context.getBean(MainFrameController.class);
     controller.setStage(primaryStage);
     ConfImpl conf = context.getBean(ConfImpl.class);
@@ -37,5 +46,40 @@ public class StockUpdater extends Application {
     primaryStage.setTitle(String.format("%s - %s", conf.getApplicationName(), conf.getVersion()));
     primaryStage.show();
   }
+
+  private void exceptionHandler(final Thread t, final Throwable e) {
+    // FIXME update the text in the dialog
+    logger.error("Oups... un problème dans le thread {}: {}", t.getName(), e.getMessage(), e);
+    Alert alert = new Alert(Alert.AlertType.ERROR);
+    alert.setTitle("Exception Dialog");
+    alert.setHeaderText("Look, an Exception Dialog");
+    alert.setContentText("Could not find file blabla.txt!");
+
+    // Create expandable Exception.
+    StringWriter sw = new StringWriter();
+    PrintWriter pw = new PrintWriter(sw);
+    e.printStackTrace(pw);
+    String exceptionText = sw.toString();
+
+    Label label = new Label("The exception stacktrace was:");
+
+    TextArea textArea = new TextArea(exceptionText);
+    textArea.setEditable(false);
+    textArea.setWrapText(true);
+
+    textArea.setMaxWidth(Double.MAX_VALUE);
+    textArea.setMaxHeight(Double.MAX_VALUE);
+    GridPane.setVgrow(textArea, Priority.ALWAYS);
+    GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+    GridPane expContent = new GridPane();
+    expContent.setMaxWidth(Double.MAX_VALUE);
+    expContent.add(label, 0, 0);
+    expContent.add(textArea, 0, 1);
+
+    // Set expandable Exception into the dialog pane.
+    alert.getDialogPane().setExpandableContent(expContent);
+
+    alert.showAndWait();  }
 
 }
